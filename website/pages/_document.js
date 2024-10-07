f31bbe642564a2c1fc4ac2cc726738324092fb24
ParthSali <parthsali04@gmail.com>
2024-10-07 19:36:07 +0530

    Update gitignore

--- website/pages/_document.js
+++ website/pages/_document.js
@@ -4,7 +4,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           {/* Google Analytics */}
           <script
