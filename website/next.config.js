5202c2b4d8e4f9122a36a2f7d6df15aa5d37288c
ParthSali <parthsali04@gmail.com>
2024-10-23 10:36:03 +0530

    Add DM unitwise notes

--- website/next.config.js
+++ website/next.config.js
@@ -1,6 +1,14 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  latex: {
+    renderer: "katex",
+    options: {
+      macros: {
+        "\\RR": "\\mathbb{R}",
+      },
+    },
+  },
+});
 
-module.exports = withNextra()
+module.exports = withNextra();
